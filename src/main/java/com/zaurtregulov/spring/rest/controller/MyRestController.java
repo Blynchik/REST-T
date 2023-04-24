e5826a31a179f68cb2e43a557917ea3799cbb88a
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-04-24 13:52:19 +0300

    6.13

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
@@ -1,12 +1,9 @@
 package com.zaurtregulov.spring.rest.controller;
 
 import com.zaurtregulov.spring.rest.entity.Employee;
-import com.zaurtregulov.spring.rest.exception_handling.EmployeeIncorrectData;
 import com.zaurtregulov.spring.rest.exception_handling.NotSuchEmployeeException;
 import com.zaurtregulov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -48,5 +45,19 @@ public class MyRestController {
         employeeService.saveEmployee(employee);
 
         return employee;
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NotSuchEmployeeException("No employee " + id);
+        }
+
+        employeeService.deleteEmployee(id);
+
+        return "Employee " + id + " deleted";
     }
 }
