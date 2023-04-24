2c5453e43f4b95ba47b9f76260d8e4aeb14c99d5
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-04-24 12:58:12 +0300

    6.11

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
@@ -19,15 +19,15 @@ public class MyRestController {
     private EmployeeService employeeService;
 
     @GetMapping("/employees")
-    public List<Employee> showAllEmployee(){
+    public List<Employee> showAllEmployee() {
         return employeeService.getAllEmployees();
     }
 
     @GetMapping("/employees/{id}")
-    public Employee getEmployee(@PathVariable int id){
+    public Employee getEmployee(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
 
-        if(employee==null){
+        if (employee == null) {
             throw new NotSuchEmployeeException("No employee id " + id);
         }
 
@@ -35,7 +35,15 @@ public class MyRestController {
     }
 
     @PostMapping("/employees")
-    public Employee addNewEmployee(@RequestBody Employee employee){
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
 
         employeeService.saveEmployee(employee);
 
